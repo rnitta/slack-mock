2ddc4ddb57951fb60d08b4b1aa5e8c91e5376686
RN <attinyes@gmail.com>
2017-11-26 08:36:54 +0900

    [fix] email_confirmationコントローラをリファクタリング

--- app/controllers/email_confirmations_controller.rb
+++ app/controllers/email_confirmations_controller.rb
@@ -2,13 +2,12 @@
 class EmailConfirmationsController < ApplicationController
   def new
     # 同じメールアドレスの既存のレコードを削除
-    EmailConfirmation.where(address: email_params).delete_all
+    EmailConfirmation.where(address: params[:email]).delete_all
     # メールアドレス確認用
     emailconfirmation = EmailConfirmation.new
-    emailconfirmation[:address] = email_params
-    emailconfirmation[:code] = (rand(900_000) + 100_000).to_s
-    emailconfirmation[:token] = SecureRandom.uuid
-    p emailconfirmation
+    emailconfirmation.address = params[:email]
+    emailconfirmation.code = (rand(900_000) + 100_000).to_s
+    emailconfirmation.token = SecureRandom.uuid
     if emailconfirmation.save
       # コードメール送信
       EmailConfirmationMailer.send_confirm(emailconfirmation).deliver_later
@@ -18,21 +17,12 @@ class EmailConfirmationsController < ApplicationController
   end
 
   def check_code
-    emailconfirmation = EmailConfirmation.find_by(address: email_params.downcase)
+    emailconfirmation = EmailConfirmation.find_by(address: params[:email].downcase)
     p emailconfirmation
-    if code_params == emailconfirmation[:code]
+    if params[:code] == emailconfirmation[:code]
       render json: { success: true, token: emailconfirmation[:token] }
     else
       render json: { success: false }
     end
-  end
-
-  private
-
-  def email_params
-    params.require(:email)
-  end
-  def code_params
-    params.require(:code)
   end
 end
