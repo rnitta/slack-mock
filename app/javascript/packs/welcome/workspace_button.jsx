d038499cab90ccbb40d99920af11ab0727912f3d
RN <attinyes@gmail.com>
2017-12-02 18:49:45 +0900

    [fix] メニュー(WSmenu)を表示するのに再レンダーしていたのを修正

--- app/javascript/packs/welcome/workspace_button.jsx
+++ app/javascript/packs/welcome/workspace_button.jsx
@@ -4,16 +4,23 @@ import ReactDOM from 'react-dom'
 import WSmenu from './workspace_menu.jsx'
 
 export default class Button extends React.Component {
+  constructor(props) {
+    super(props)
+    // stateの初期値を設定
+    this.state = {
+      style_wsmenu: "none"
+    }
+  }
   expandMenu() {
-    ReactDOM.render(
-      <WSmenu/>,
-    document.body.appendChild(document.createElement('div'))
-    );
+    this.setState({style_wsmenu: "block"})
   }
 
   render() {
     return (
-      <button className={this.props.classname} onClick={this.expandMenu.bind(this)}>{this.props.text}</button>
+      <div>
+        <button className={this.props.classname} onClick={this.expandMenu.bind(this)}>{this.props.text}</button>
+        <WSmenu style={this.state.style_wsmenu}/>
+      </div>
     );
   }
 }
--- app/javascript/packs/welcome/workspace_menu.jsx
+++ app/javascript/packs/welcome/workspace_menu.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default class WSmenu extends React.Component {
   render() {
-    return(
-      <div  className="workspace_menu">
+    return (
+      <div className="workspace_menu" style={{
+        display: this.props.style
+      }}>
         <a href="/new_workspace" target="_blank">Create New Workspace</a>
       </div>
     );
