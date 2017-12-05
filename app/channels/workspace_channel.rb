a5a698be2a8851054b444cc6a07492e3cd58871b
RN <attinyes@gmail.com>
2017-12-06 06:04:24 +0900

    [add] 購読するチャンネル名を設定

--- app/channels/workspace_channel.rb
+++ app/channels/workspace_channel.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class WorkspaceChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from current_user.token
   end
 
   def unsubscribed
