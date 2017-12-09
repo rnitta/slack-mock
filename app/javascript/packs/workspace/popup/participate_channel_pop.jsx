3fd892992df7469de8296c0b1a19503114d3164b
RN <attinyes@gmail.com>
2017-12-10 05:09:30 +0900

    [add] チャンネル参加画面の概形を追加

---
+++ app/javascript/packs/workspace/popup/participate_channel_pop.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class ParticipateChannelPop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  hide() {
+    this.props.update_state({participate_channel_pop: false})
+  }
+  render() {
+    return (
+      <div id="participate_channel_pop">
+        <div className="pop_container">
+          <h1>Join channels</h1>
+          <div id="right_up">
+            <button className="close_btn" onClick={this.hide.bind(this)}>×</button>
+          </div>
+          <form>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- app/javascript/packs/workspace/side_container.jsx
+++ app/javascript/packs/workspace/side_container.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import axios from 'axios'
 
 import CreateChannelPop from './popup/create_channel_pop.jsx'
+import ParticipateChannelPop from './popup/participate_channel_pop.jsx'
 export default class SideContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       channels: {},
+      create_channel_pop: false,
       create_channel_pop: false
     }
     this.workspace_data()
@@ -27,6 +29,9 @@ export default class SideContainer extends React.Component {
   pop_create_channel(){
     this.setState({create_channel_pop: true})
   }
+  pop_participate_channel(){
+    this.setState({participate_channel_pop: true})
+  }
   update_state(obj){
     this.setState(obj)
   }
@@ -37,13 +42,14 @@ export default class SideContainer extends React.Component {
         <div id="starred_menu"></div>
         <div id="channel_menu">
           <p className="menu_line">
-            <button id="channels_button">Channels</button>
+            <button id="channels_button" onClick={this.pop_participate_channel.bind(this)}>Channels</button>
             <button id="create_channel_button" onClick={this.pop_create_channel.bind(this)}>+</button>
           </p>
         </div>
         <div id="dm_menu"></div>
         <div id="bottom_menu"></div>
-        {(()=>{if(this.state.create_channel_pop){return <CreateChannelPop update_state={this.update_state.bind(this)} grandparentstate={this.props.parentstate} />}})()}
+          {(()=>{if(this.state.create_channel_pop){return <CreateChannelPop update_state={this.update_state.bind(this)} grandparentstate={this.props.parentstate} />}})()}
+          {(()=>{if(this.state.participate_channel_pop){return <ParticipateChannelPop update_state={this.update_state.bind(this)} grandparentstate={this.props.parentstate} />}})()}
       </div>
     );
   }
