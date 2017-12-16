1be8cbb61edbc57f7d9a7ffa07be26a93404ac6e
RN <attinyes@gmail.com>
2017-12-16 12:40:24 +0900

    [add] ☆★を切り替える

--- app/javascript/packs/workspace/main/channel_header.jsx
+++ app/javascript/packs/workspace/main/channel_header.jsx
@@ -7,13 +7,20 @@ export default class ChannelHeader extends React.Component {
     this.state = {
     }
   }
+  click_star(e){
+    if(e.target.innerText == "☆"){
+    e.target.innerText="★"
+  }else{
+    e.target.innerText="☆"
+    }
+  }
   render() {
     return (
       <div id="channel_header_container">
         <div className="header_left">
           <span className="channel_name_span"># {this.props.grandparentstate.selected_channel}</span>
           <div className="channel_header_info">
-            <button className="star_btn">☆</button>
+            <button className="star_btn" onClick={this.click_star.bind(this)}>☆</button>
             <button className="members_count_btn">n</button>
             <button className="edit_topic_btn">トピックを編集</button>
           </div>
