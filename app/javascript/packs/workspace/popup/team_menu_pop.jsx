fd659a130c7f50816417f5e1d7bf70532b266008
RN <attinyes@gmail.com>
2017-12-18 15:22:08 +0900

    [add] ワークスペースメニューの中身を追加

--- app/javascript/packs/workspace/popup/team_menu_pop.jsx
+++ app/javascript/packs/workspace/popup/team_menu_pop.jsx
@@ -16,8 +16,17 @@ export default class TeamMenuPop extends React.Component {
       <div id="team_menu_pop">
         <div id="team_menu_outer" onClick={this.hide_pop.bind(this)}>
           <div id="team_menu_inner">
-            <div id="user_menu_container"></div>
-            <div id="workspace_menu_container"></div>
+            <div id="user_menu_container">
+              <div id="user_menu_header">
+                <p id="your_display_name">{this.props.grandparentstate.display_name}</p>
+              </div>
+            </div>
+            <div id="workspace_menu_container">
+              <div id="workspace_menu_header">
+                <p id="current_team_name">{this.props.grandparentstate.workspace_name}</p>
+                <p id="current_team_domain">({this.props.grandparentstate.domain})</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
