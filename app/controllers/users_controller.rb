9ea9cbeeb350cd506d8499e877b9773f99b3eaaa
RN <attinyes@gmail.com>
2017-12-19 18:33:24 +0900

    [add] ルーティングを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
+  def invited
+    redirect_to root_path unless Invitation.exists?(token: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post '/channels/join', to: 'channels#join'
   post '/channels/star', to: 'channels#star'
   post '/channels/topic', to: 'channels#topic'
+  get '/users/invite/:id', to: 'users#invited', constraints: { id: %r([^\/]+) }
 end
