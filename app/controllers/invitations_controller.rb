7ab905659594c6b6d33e5d36f31ea36fbce2af3a
RN <attinyes@gmail.com>
2017-12-19 20:00:54 +0900

    [fix] JWTのデコード部分を分離するリファクタリング

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 class InvitationsController < ApplicationController
   def data
-    decoded_token = JWT.decode(params[:jwt], Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
-    email = decoded_token[0]['email']
-    workspace_id = decoded_token[0]['workspace_id']
-    user_id = decoded_token[0]['user_id']
-    workspace = Workspace.find(workspace_id)
-    user = User.find(user_id)
+    workspace = Workspace.find(decoded_token.workspace_id)
+    user = User.find(decoded_token.user_id)
     render json: { success: true,
-                   email: email,
+                   email: decoded_token.email,
                    workspace_name: workspace.name,
                    domain: workspace.domain,
                    invitor: user.display_name }
+  end
+
+  private
+
+  def decoded_token
+    decoded_token = JWT.decode(params[:jwt], Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+    dot_hash = ActiveSupport::OrderedOptions.new
+    dot_hash.email = decoded_token[0]['email']
+    dot_hash.workspace_id = decoded_token[0]['workspace_id']
+    dot_hash.user_id = decoded_token[0]['user_id']
+    dot_hash
   end
 end
