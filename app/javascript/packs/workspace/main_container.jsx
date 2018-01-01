80c43cd5f966728aa0c090bef93be3dddcb02831
RN <attinyes@gmail.com>
2018-01-02 05:17:43 +0900

    [add] メッセージ履歴をPOSTで要求してstateに保存するJSを追加

--- app/javascript/packs/workspace/main_container.jsx
+++ app/javascript/packs/workspace/main_container.jsx
@@ -6,35 +6,64 @@ import MessageForm from './main/message_form.jsx'
 export default class MainContainer extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
+    this.state = {}
+  }
+  componentDidMount() {
+    this.get_message_list()
+  }
+  componentWillReceiveProps() {
+    this.get_message_list()
+  }
+  get_message_list() {
+    let type = this.props.parentstate.selected
+    if (type == "user") {
+      let name = this.props.parentstate.selected_user.name
+    } else if (type == "channel") {
+      let name = this.props.parentstate.selected_channel.name
     }
+    axios.defaults.headers['X-CSRF-TOKEN'] = this.props.parentstate.csrf_token
+    axios.post('/workspaces/messages', {
+      jwt: this.props.parentstate.jwt,
+      type: type,
+      name: name
+    }).then((results) => {
+      if (results.data.success) {
+        this.setState({messages: results.data.messages}, ()=>{console.log(this.state)})
+      }
+    },).catch(() => {
+      alert('エラー')
+    });
   }
   update_parent_state(obj) {
     this.props.update_state(obj)
   }
   render() {
-    if(!!this.props.parentstate.selected){
+    var message_list = []
+    if (!!this.props.parentstate.messages) {}
+    if (!!this.props.parentstate.selected) {
       var header = []
-      if(this.props.parentstate.selected == "channel"){
-          header = <ChannelHeader update_parent_state={this.update_parent_state.bind(this)} grandparentstate={this.props.parentstate}/>
-      }else if (this.props.parentstate.selected == "user") {
+      if (this.props.parentstate.selected == "channel") {
+        header = <ChannelHeader update_parent_state={this.update_parent_state.bind(this)} grandparentstate={this.props.parentstate}/>
+      } else if (this.props.parentstate.selected == "user") {
         header = <DmHeader grandparentstate={this.props.parentstate}/>
       }
-    return (
-      <div id="main_container">
-        <div id="header_column">
-          {header}
+      return (
+        <div id="main_container">
+          <div id="header_column">
+            {header}
+          </div>
+          <div id="message_column">
+            {message_list}
+          </div>
+          <div id="fixed_column">
+            <MessageForm grandparentstate={this.props.parentstate}/>
+          </div>
         </div>
-        <div id="message_column">
-
-        </div>
-        <div id="fixed_column">
-          <MessageForm grandparentstate={this.props.parentstate}/>
-        </div>
-      </div>
-    );
-  }else{
-    return (<p>select something</p>)
-  }
+      );
+    } else {
+      return (
+        <p>select something</p>
+      )
+    }
   }
 }
